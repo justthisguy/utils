862d9d59b3ce482ef9359b7ce26f0aac34423dab
just this guy <github.com@justthisguy.org>
2013-03-13 21:45:33 -0700

    chunked!

--- spec/word_chunker_spec.rb
+++ spec/word_chunker_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require_relative '../word_chunker'
 
 # p "the quick brown fox jumped over the lazy brown dog".split_by_chunk_size(10)
@@ -7,16 +9,56 @@ require_relative '../word_chunker'
 describe WordChunker do
 
 	it "returns the word if there is only one word" do
-		chunks = WordChunker.new "quick", 6
-		chunks.should == ["quick"]
+		chunker = WordChunker.new "quick", 6
+		chunker.chunk.should == ["quick"]
 	end
 
-	#
-  #
-  #it "words with lenght 1 less than limit breaks into individual chunks" do
-  #  chunks = WordChunker.new "quick brown", 6
-		#chunks.count.should == 2
-  #end
+  it "words with lenght 1 less than limit breaks into individual chunks" do
+    chunker = WordChunker.new "quick brown", 6
+		chunker.chunk.should == ["quick", "brown"]
+	end
 
+	it "small test" do
+   chunker = WordChunker.new "the quick brown fox ", 10
+	 chunker.chunk.should ==  ["the quick", "brown fox"]
+  end
 
+	it "big test" do
+   chunker = WordChunker.new "the quick brown fox jumped over the lazy brown dog", 11
+	 chunker.chunk.should ==  ["the quick", "brown fox", "jumped over", "the lazy", "brown dog"]
+  end
+
+	it "doesn't split spaces into chunks" do
+   chunker = WordChunker.new "the       quick       brown      fox", 10
+	 chunker.chunk.should ==  ["the quick", "brown fox"]
+  end
+
+	it "if the word is longer than the chunk, shorten word using ellipse - only" do
+   chunker = WordChunker.new "antidisestabilishmentterianism", 22
+	 chunker.chunk.should ==  ["antidisestabilishment…"]
+  end
+
+	it "if the word is longer than the chunk, shorten word using ellipse - last" do
+   chunker = WordChunker.new "bob loves antidisestabilishmentterianism", 22
+	 chunker.chunk.should ==  ["bob loves", "antidisestabilishment…"]
+	end
+
+	it "if the word is longer than the chunk, shorten word using ellipse middle" do
+		chunker = WordChunker.new "bob loves antidisestabilishmentterianism every day", 22
+		chunker.chunk.should ==  ["bob loves", "antidisestabilishment…", "every day"]
+	end
+
+	describe "class methods" do
+		it "chunks the string with a class method" do
+			chunks = WordChunker.chunk "the quick brown fox jumped over the lazy brown dog", 11
+	 		chunks.should ==  ["the quick", "brown fox", "jumped over", "the lazy", "brown dog"]
+		end
+	end
+
+	describe "String method" do
+		it "chunks the string with a class method" do
+			chunks = "the quick brown fox jumped over the lazy brown dog".split_by_chunk_size 11
+	 		chunks.should ==  ["the quick", "brown fox", "jumped over", "the lazy", "brown dog"]
+		end
+	end
 end
--- word_chunker.rb
+++ word_chunker.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class WordChunker
 
   def initialize(str, limit)
@@ -6,36 +8,56 @@ class WordChunker
   end
 
   def chunk
-    stack = []
+    @stack = []
     @current_chunk = ""
-    p "#chunked:words", words
     words.each do |word|
-      if add_to_existing?(word)
-        @current_chunk << " " + word
+      if add_this_word_to_current_chunk?(word)
+        @current_chunk << ( @current_chunk.empty? ? "" : " " ) + word
       else
-        stack << @current_chunk
-        @current_chunk = ""
+				add_to_chunk(@current_chunk)
+        @current_chunk = make_word_addable(word)
       end
-    end
-    stack
-  end
+		end
+		add_to_chunk(@current_chunk)
+	end
 
-  def add_to_existing?(word)
-    @current_chunk.length + word.length + 1  <= @limit
-  end
+	def self.chunk str, limit
+		chunker = self.new(str, limit)
+		chunker.chunk
+	end
+
+	private
+
+  def add_this_word_to_current_chunk?(word)
+    @current_chunk.length + word.length + 1 <= @limit
+	end
+
+	def add_to_chunk(chunk)
+		if !chunk.empty?
+			@stack << chunk
+		else
+			@stack
+		end
+	end
+
 
   def words
     @string.split(/\s+/)
-  end
+	end
+
+	def make_word_addable (word)
+		if word.length  <= @limit
+			word
+		else
+			word[0..@limit-2] + "…"
+		end
+	end
 end
-
-
-
 
 
 class String
   def split_by_chunk_size(limit)
-    chunkie = WordChunker.new(self, limit)
-    chunkie.chunked
+		chunker = WordChunker.new(self, limit)
+		chunker.chunk
   end
 end
